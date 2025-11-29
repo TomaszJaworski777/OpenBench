a0c00b267c091b5fba3f7264ec3e8a55953c2bec
TomaszJaworski777 <71073337+TomaszJaworski777@users.noreply.github.com>
2025-11-29 23:01:50 +0100

    Finalized test history

--- OpenBench/static/copy_text.js
+++ OpenBench/static/copy_text.js
@@ -22,3 +22,98 @@ function copy_text(element_id, keep_url) {
         console.error("Unable to copy to Clipboard");
     }
 }
+
+function copy_text_from_test(test) {
+    function align_to_center(text, len=44) {
+        if ( text.length >= len ) {
+            return text;
+        }
+
+        space_size = len - text.length;
+        modulo = " ".repeat(space_size % 2);
+        space = " ".repeat(space_size / 2);
+        return space + text + space + modulo;
+    }
+
+    function align_to_right(text, len=13) {
+        if ( text.length >= len ) {
+            return text;
+        }
+
+        space = " ".repeat(len - text.length);
+        return space + text;
+    }
+
+    function align_to_left(text, len=16) {
+        if ( text.length >= len ) {
+            return text;
+        }
+
+        space = " ".repeat(len - text.length);
+        return text + space;
+    }
+
+    const pairs = test.options.split(' ');
+    hash = "";
+    threads = "";
+
+    pairs.forEach(pair => {
+        const [key, value] = pair.split('=');
+
+        if(key == "Hash") {
+            if(parseInt(value) > 1024) {
+                hash = (parseInt(value) / 1024).toFixed(1) + "GB";
+                return;
+            }
+            hash = value + "MB";
+            return;
+        }
+
+        if(key == "Threads") { 
+            threads = value;
+        }
+    });
+
+    result = "";
+
+    result += align_to_center(test.title) + "\n";
+    result += align_to_left("TC: " + test.tc + "s", 16) + "|" + align_to_center("Hash: " + hash, 14) + "|" + align_to_right("Threads: " + threads, 12) + "\n";
+    result += "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n";
+
+    switch(test.mode) {
+        case 'SPRT': {
+            result += "Elo     : " + test.elo + " (95%)\n";
+            result += "LLR     : " + test.llr + "\n";
+            result += "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n";
+            wr = (100 * parseInt(test.w) / (parseInt(test.w) + parseInt(test.l))).toFixed(1);
+            result += "Games   : " + test.games + " (" + wr + "%)\n";
+            result += "W-D-L   : +" + test.w + " =" + test.d + " -" + test.l + "\n";
+            result += "Penta   : " + test.penta + "\n";
+            break; 
+        }
+        case 'GAMES': {
+            result += "Elo     : " + test.elo + " (95%)\n";
+            wr = (100 * parseInt(test.w) / (parseInt(test.w) + parseInt(test.l))).toFixed(1);
+            result += "Games   : " + test.games + " / " + test.max_games + " (" + wr + "%)\n";
+            result += "W-D-L   : +" + test.w + " =" + test.d + " -" + test.l + "\n";
+            result += "Penta   : " + test.penta + "\n";
+            break;
+        }
+    }
+
+    result += "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n";
+    result += align_to_center(window.location.origin + test.url);
+
+    if (navigator.clipboard && window.isSecureContext) {
+        navigator.clipboard.writeText(result).then(() => {
+            console.log("Copied!");
+        }).catch(err => console.error(err));
+    } else {
+        var area = document.createElement("textarea");
+        area.value = result;
+        document.body.append(area);
+        area.select();
+        document.execCommand("copy");
+        document.body.removeChild(area);
+    }
+}
